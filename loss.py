3c816e647f4545428e90edfce3b24b5189755bc0
杜宏泰 <2535692722@qq.com>
2026-04-15 12:48:49 +0800

    4-15

--- loss.py
+++ loss.py
@@ -8,7 +8,7 @@ class FeatureLoss(nn.Module):
 
         # 位置信息用MSE，类别用交叉熵
         self.location_loss = nn.MSELoss()
-        self.class_loss = nn.CrossEntropyLoss()
+        self.class_loss = nn.CrossEntropyLoss()# 里面包含softmax
 
     # predict 尺寸[batch_size, 8]
     def forward(self, predicts, targets):
@@ -16,13 +16,17 @@ class FeatureLoss(nn.Module):
         location_loss = self.location_loss(predicts[:, 0:4], targets[:, 0:4])
         class_loss = self.class_loss(predicts[:, 4:8], targets[:, 4:8])
 
-        return location_loss, class_loss
+        return location_loss+class_loss
 
 if __name__ == '__main__':
     loss = FeatureLoss()
-    predicts = torch.rand(1,8)
+    predicts = torch.tensor([
+        [0.4, 0.3, 0.2, 0.1,9.0, -1.4, 0.3, 0.2],
+        [0.3, 0.2, 0.1, 0.5, 1.0, 0.3, 4.0, 0.4]
+    ])
     targets = torch.tensor([
-        [0.5, 0.6, 1.2, 0.3, 1.0, 0.0, 0.0, 0.0]
+        [0.3, 0.2, 0.1, 0.2, 1.0, 0.0, 0.0, 0.0],
+        [0.4, 0.2, 0.1, 0.2, 0.0, 1.0, 0.0, 0.0]
     ])
     print(predicts.shape)
     print(targets.shape)
