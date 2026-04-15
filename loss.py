adfb7a13e9160e55d3c8a865cf94791bf6614339
杜宏泰 <2535692722@qq.com>
2026-04-15 11:14:29 +0800

    4-15

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -12,4 +13,17 @@ class FeatureLoss(nn.Module):
     # predict 尺寸[batch_size, 8]
     def forward(self, predicts, targets):
 
-        location_loss = self.location_loss(predicts, targets)
+        location_loss = self.location_loss(predicts[:, 0:4], targets[:, 0:4])
+        class_loss = self.class_loss(predicts[:, 4:8], targets[:, 4:8])
+
+        return location_loss, class_loss
+
+if __name__ == '__main__':
+    loss = FeatureLoss()
+    predicts = torch.rand(1,8)
+    targets = torch.tensor([
+        [0.5, 0.6, 1.2, 0.3, 1.0, 0.0, 0.0, 0.0]
+    ])
+    print(predicts.shape)
+    print(targets.shape)
+    print(loss(predicts, targets))
